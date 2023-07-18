301969e80e1df7370801e8a19d7e5bacfb767650
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-07-17 19:22:19 -0700

    Updated Food Classifier 101

---
+++ demos/food_classifier_101/app.py
@@ -0,0 +1,81 @@
+### 1. Imports and class names setup ### 
+import gradio as gr
+import os
+import torch
+
+from model import create_effnetb2_model
+from timeit import default_timer as timer
+from typing import Tuple, Dict
+
+# Setup class names
+with open("class_names.txt", "r") as f: # reading them in from class_names.txt
+    class_names = [food_name.strip() for food_name in  f.readlines()]
+    
+### 2. Model and transforms preparation ###    
+
+# Create model
+effnetb2, effnetb2_transforms = create_effnetb2_model(
+    num_classes=101, # could also use len(class_names)
+)
+
+# Load saved weights
+effnetb2.load_state_dict(
+    torch.load(
+        f="Food_101.pth",
+        map_location=torch.device("cpu"),  # load to CPU
+    )
+)
+
+### 3. Predict function ###
+
+# Create predict function
+def predict(img) -> Tuple[Dict, float]:
+    """Transforms and performs a prediction on img and returns prediction and time taken.
+    """
+    # Start the timer
+    start_time = timer()
+    
+    # Transform the target image and add a batch dimension
+    img = effnetb2_transforms(img).unsqueeze(0)
+    
+    # Put model into evaluation mode and turn on inference mode
+    effnetb2.eval()
+    with torch.inference_mode():
+        # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+        pred_probs = torch.softmax(effnetb2(img), dim=1)
+    
+    # Create a prediction label and prediction probability dictionary for each prediction class (this is the required format for Gradio's output parameter)
+    pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
+    
+    # Calculate the prediction time
+    pred_time = round(timer() - start_time, 5)
+    
+    # Return the prediction dictionary and prediction time 
+    return pred_labels_and_probs, pred_time
+
+### 4. Gradio app ###
+
+# Create title, description and article strings
+title = "Food Classifier"
+description = "An EfficientNetB2 feature extractor computer vision model to classify images of food."
+article = "Created by Myles Penner (https://github.com/MylesJP)"
+
+# Create examples list from "examples/" directory
+example_list = [["examples/" + example] for example in os.listdir("examples")]
+
+# Create Gradio interface 
+demo = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs=[
+        gr.Label(num_top_classes=5, label="Predictions"),
+        gr.Number(label="Prediction time (s)"),
+    ],
+    examples=example_list,
+    title=title,
+    description=description,
+    article=article,
+)
+
+# Launch to Gradio
+demo.launch()
